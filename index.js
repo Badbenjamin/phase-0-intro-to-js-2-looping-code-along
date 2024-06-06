3b76a8545f6fca6013b75e3f28ac875b279fe7c4
ben mccarthy <bmccarthy90@gmail.com>
2024-06-06 18:19:17 -0400

    Passing Looping Lab Tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+function writeCards(array, party) {
+    const messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${party} gift!`);
+        debugger;
+    }
+
+    return messages;
+}
+
+// const result = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+
+}
+
+let result = countDown(10);
+
+
+console.log(result);
